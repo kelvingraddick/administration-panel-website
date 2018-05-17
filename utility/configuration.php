b74aba870fe45b7c8d30cb4bf3a9e207e34a35a3
Kelvin <kgraddick@wavelinkllc.com>
2018-05-16 20:53:50 -0400

    Updated configuration template

--- utility/configuration.php
+++ utility/configuration.php
@@ -1,9 +1,10 @@
 <?php
 	$site_name = "Site Name";
-	$site_image = "/images/logo.png";
 
 	$database_host = "localhost";
 	$database_username = "username";
 	$database_password = "password";
 	$database_name = "database";
+
+	$_ENV["PROTOCOL"] = "http://";
 ?>
