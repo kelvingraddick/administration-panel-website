58ff83854f17b5338da546b08edbc4fb2cfd2dc9
Kelvin <kgraddick@wavelinkllc.com>
2018-09-10 21:43:30 -0400

    Fixing issue with saving order for Employees

--- employees/submit.php
+++ employees/submit.php
@@ -14,11 +14,12 @@
 	$email_address = addslashes($_POST['email_address']);
     $phone_number = addslashes($_POST['phone_number']);
 	$description = addslashes($_POST['description']);
+	$order_index = addslashes($_POST['order_index']);
 
     $response = array();
 
 	if ($id == "") {
-		if (mysqli_query($database_connection, "INSERT INTO employees (first_name, last_name, email_address, phone_number, description) values('$first_name', '$last_name', '$email_address', '$phone_number', '$description')")) {
+		if (mysqli_query($database_connection, "INSERT INTO employees (first_name, last_name, email_address, phone_number, description, order_index) values('$first_name', '$last_name', '$email_address', '$phone_number', '$description', '$order_index')")) {
 			$response['success'] = true;
 		} else {
             $response['success'] = false;
@@ -26,7 +27,7 @@
 		}
 	} else {
 		$updated_time = date("Y-m-d H:i:s");
-		if (mysqli_query($database_connection, "UPDATE employees SET first_name = '$first_name', last_name = '$last_name', email_address = '$email_address', phone_number = '$phone_number', description = '$description', updated_time = '$updated_time' where id = '$id'")){ 
+		if (mysqli_query($database_connection, "UPDATE employees SET first_name = '$first_name', last_name = '$last_name', email_address = '$email_address', phone_number = '$phone_number', description = '$description', order_index = '$order_index', updated_time = '$updated_time' where id = '$id'")){ 
 			$response['success'] = true;
 		} else {
             $response['success'] = false;
