bec277a5b324474b3dadbd37278d0af0dc626477
Kelvin Graddick <kelvingraddick@kg.codes>
2020-09-18 02:56:01 -0400

    Send email when vendor is approved

--- utility/common.php
+++ utility/common.php
@@ -100,4 +100,121 @@
 		}
 		return $file_ary;
 	}
+
+	function send_email($parameters) {
+        $url = "https://api.sendgrid.com/v3/mail/send";
+        $content = '{
+            "personalizations": [
+                {
+                    "to": [
+                        {
+                            "email": '.json_encode($parameters['recipient_email_address']).',
+                            "name": '.json_encode($parameters['recipient_name']).'
+                        }
+					],
+                    "subject": '.json_encode($parameters['subject']).'
+                }
+            ],
+            "from": {
+                "email": "notifications@cutcornersapp.com",
+                "name": "Cut Corners"
+            },
+			"content": [
+                {
+                    "type": "text/html",
+                    "value": '.json_encode($parameters['body']).'
+                }
+            ]
+        }';
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-type: application/json", "Authorization: Bearer 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"));
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+		$curl_response = json_decode(curl_exec($curl));
+        $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        if ($status != 202) { 
+			$curl_response -> error_message = "Error: call to URL $url failed with status $status, response ".json_encode($curl_response).", curl_error ".curl_error($curl).", curl_errno ".curl_errno($curl);
+		}
+		curl_close($curl);
+		return $curl_response;
+	}
+
+	function get_email_template($content, $setting) {
+        $template = '
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="initial-scale=1.0">
+        <meta name="format-detection" content="telephone=no">
+        <style type="text/css">
+            table {
+                border-spacing: 0;
+            }
+            table td {
+                border-collapse: collapse;
+            }
+            @media screen and (max-width: 600px) {
+                table[class="container"] {
+                    width: 95% !important;
+                }
+            }
+            @media screen and (max-width: 480px) {
+                td[class="container-padding"] {
+                    padding-left: 12px !important;
+                    padding-right: 12px !important;
+                }
+            }
+            @media only screen and (max-width : 600px) {
+                td[class="force-col"] {
+                    display: block;
+                    padding-right: 0 !important;
+                }
+            }
+        </style>
+        <table border="0" width="100%" height="100%" cellpadding="0" cellspacing="0" bgcolor="#ebebeb">
+            <tbody>
+                <tr>
+                    <td align="center" valign="top" bgcolor="#ebebeb" style="background-color: #ebebeb;">
+                        <br><br>
+                        <table border="0" width="600" cellpadding="0" cellspacing="0" class="container" bgcolor="#ffffff">
+                            <tbody>
+                                <tr>
+                                    <td class="container-padding" bgcolor="#ffffff" 
+                                        style="background-color: #ffffff; padding-left: 30px; padding-right: 30px; font-size: 14px; line-height: 20px; font-family: Helvetica, sans-serif; color: #333;-moz-box-shadow: 3px 3px 3px 3px #ccc; -webkit-box-shadow: 3px 3px 3px 3px #ccc; box-shadow: 3px 3px 3px 3px #ccc;&nbsp;border-radius:10px;">
+                                        <br>
+                                        <img src="{logo_url}" width="25%">
+                                        <br> 
+                                        <table border="0" cellpadding="0" cellspacing="0">
+                                            <tbody>
+                                                <tr>
+                                                    <td class="force-col" style="background-color: #ffffff; font-size: 13px; line-height: 20px; font-family: Helvetica, sans-serif; color: #333;" valign="top">
+                                                        <br>
+                                                        {content}                                
+                                                        <br><br>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <br />
+                        <span style="font-size: 12px; font-family: Helvetica, sans-serif; color: #333;"><a href="http://www.cutcornersapp.com/email/unsubscribe/">Unsubscibe</a></span>
+                        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <br>
+        <br>';
+        $template = str_replace("{logo_url}", $setting['logo'], $template);
+        $template = str_replace("{content}", $content, $template);
+        $template = str_replace("{facebook_link}", $setting['facebook_link'], $template); 
+        $template = str_replace("{instagram_link}", $setting['instagram_link'], $template);
+        $template = str_replace("{twitter_link}", $setting['twitter_link'], $template);
+        $template = str_replace("{linkedin_link}", $setting['linkedin_link'], $template);
+        return $template;
+    }
 ?>
--- vendors/submit.php
+++ vendors/submit.php
@@ -34,8 +34,29 @@
             $response['error_message'] = "There was an error saving: ".mysqli_error($database_connection);
 		}
 	} else {
+		$vendor = mysqli_fetch_assoc(mysqli_query($database_connection, "SELECT * FROM vendors WHERE id = '$id' LIMIT 1"));
+		$is_already_enabled = ($vendor != null) ? $vendor['is_enabled'] : 0;
+
 		if (mysqli_query($database_connection, "UPDATE vendors SET user_id = '$user_id', type = '$type', business_name = '$business_name', description = '$description', location_name = '$location_name', latitude = '$latitude', longitude = '$longitude', logo_url = '$logo_url', banner_image_url = '$banner_image_url', website_url = '$website_url', instagram_handle = '$instagram_handle', facebook_handle = '$facebook_handle', handclaps = '$handclaps', is_enabled = '$is_enabled' where id = '$id'")){ 
 			$response['success'] = true;
+			if ($is_already_enabled == 0 && $is_enabled == 1) {
+				$user = mysqli_fetch_assoc(mysqli_query($database_connection, "SELECT * FROM users WHERE id = '$user_id' LIMIT 1"));
+				if ($user != null) {
+					$email = array();
+					$email['recipient_email_address'] = $user['email_address'];
+					$email['recipient_name'] = $user['first_name'].' '.$user['last_name'];
+					$email['subject'] = "Time to complete your registration for Cut Corners!";
+					$content = 'Thank you for signing up for Cut Corners as a hair professional!<br />
+					<b>Your account has been approved and is ready for you to complete your registration!</b><br />
+					Please log into the app, go to the account tab, then \'Add/Edit Subscription\', to subscribe and complete the registration.<br />
+					<br />
+					Welcome!<br />
+					- Cut Corners team';
+					$settings = get_settings($database_connection);
+					$email['body'] = get_email_template($content, $settings);
+					send_email($email);
+				}
+			}
 		} else {
             $response['success'] = false;
             $response['error_message'] = "There was an error saving: ".mysqli_error($database_connection);
